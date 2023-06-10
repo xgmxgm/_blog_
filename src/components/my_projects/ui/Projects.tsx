b2261209c28df574dd84625a4187e8dfab123738
xgmxgm <109265751+xgmxgm@users.noreply.github.com>
2023-06-10 15:21:25 +0600

    Blog
    Blog

--- src/components/my_projects/ui/Projects.tsx
+++ src/components/my_projects/ui/Projects.tsx
@@ -6,6 +6,7 @@ import  { Project_card }  from "@/components/project/ui/Project_card"
 
 import speede from "../../../../public/img/logo_spedee.webp"
 import type from "../../../../public/img/TYPEING.webp"
+import GenPass from "../../../../public/img/GenPass.webp"
 
 export const Projects: FC = () => {
     const { t } = useTranslation();
@@ -15,6 +16,7 @@ export const Projects: FC = () => {
             <div className="Projects__Table">
                 <Project_card id={1} img={speede} title="SPEDEE" info={t("Main.Portfolio.projects.project_1.info")} />
                 <Project_card id={2} img={type} title="TYPEING" info={t("Main.Portfolio.projects.project_2.info")} />
+                <Project_card id={3} img={GenPass} title="GenPass" info={t("Main.Portfolio.projects.project_3.info")} />
             </div>
         </div>
     )
